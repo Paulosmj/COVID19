2b3c533e88a5b5eda54d1db036f3474857a08ed0
Paulosmj <psmeneguesso@gmail.com>
2022-04-25 19:26:55 -0300

    fix config

--- src/api.js
+++ src/api.js
@@ -1,5 +1,4 @@
-/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
-const path = 'http://coronavirus-19-api.herokuapp.com/countries';
+const path = 'https://coronavirus-19-api.herokuapp.com/countries'
 
 const headers = {
     method: 'get',
