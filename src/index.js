688078f7da9a9c6c90897645e1d78b629047c603
Paulosmj <psmeneguesso@gmail.com>
2022-04-25 17:05:22 -0300

    improvements

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// ReactDOM.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
 //   <React.StrictMode>
 //     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
+//   </React.StrictMode>
 // );
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 serviceWorker.register();
